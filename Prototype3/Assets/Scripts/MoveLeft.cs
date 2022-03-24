9ec03d4370063d20dc3c77b0d34c415352f3298f
Gaye Yılmaz <gaye3545@gmail.com>
2022-03-24 13:33:21 +0300

    Background movement modified

--- Prototype3/Assets/Scripts/MoveLeft.cs
+++ Prototype3/Assets/Scripts/MoveLeft.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
+    private PlayerController playerControllerScript;
     private float speed = 30;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if(playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+        }
     }
 }
--- Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public float jumpForce = 10;
     public float gravityModifier;
+
+    public bool gameOver;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +30,16 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+
+        }
+        else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+
+        }
     }
 }
